743b4e3800c5fd4f1153bcba2745962432472b73
jefferson-s <jefferson.santos10@outlook.com>
2021-09-27 17:41:57 -0300

    building

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# project_series
+Projeto da DIO, montando uma lista de séries com comandos .Net
+
+## :desktop_computer:
+
